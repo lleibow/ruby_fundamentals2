c67db2a9f2145cf3fdfd1dbef08f39bbeae73518
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-17 17:57:40 -0500

    used student.keys to output all the key names i.e. cohort names

--- exercise7.rb
+++ exercise7.rb
@@ -7,7 +7,17 @@ students = {
 def name_number(item)
   item.each do |c, n|
     puts "#{c}:#{n}"
-end
+  end
 end
 
+puts "Names of cohorts and numbers"
+
 name_number(students)
+
+puts "List with fourth cohort"
+
+students[:cohort4] = 43
+
+name_number(students)
+
+puts students.keys
